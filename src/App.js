b57a80c331617cad0041a99ad151b21d3e00371a
magdielnavarrete <magdiel.navarrete@ugm.cl>
2021-09-21 19:50:47 -0300

    Cambio de nombre

--- src/App.js
+++ src/App.js
@@ -3,13 +3,8 @@ import './App.css';
 
 function App() {
     return ( <div className = "App" >
-        <header className = "App-header" >
-        <img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
-        <p>
-       Ecommer Magdiel Navarrete</p>
-        </header> 
+        <h1>Ecommerce Magdiel Navarrete</h1>
+        <p>Hola</p>
         </div>
     );
 }
